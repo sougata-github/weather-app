df9183b4a6be983fe30dfe8e129c9a5d541811b1
Sougata <sougatadas9874@gmail.com>
2025-05-27 22:23:10 +0530

    fix: not-found ui.

--- src/pages/not-found/NotFound.tsx
+++ src/pages/not-found/NotFound.tsx
@@ -5,14 +5,12 @@ import styles from "./notFound.module.css";
 const NotFound = () => {
   return (
     <div className={styles.notFound}>
-      <div className={styles.container}>
-        <p>The page you're looking for does not exist.</p>
+      <p>The page you're looking for does not exist.</p>
 
-        <div>
-          <Link to="/dashboard" className={styles.link}>
-            Go back to Dashboard
-          </Link>
-        </div>
+      <div>
+        <Link to="/dashboard" className={styles.link}>
+          Go back to Dashboard
+        </Link>
       </div>
     </div>
   );
